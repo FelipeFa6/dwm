14a7b8063f4447c7da7d9e7c5ad91a1ca12cb300
FelipeFa6 <felipe.farias.e1@gmail.com>
2025-05-18 00:44:54 -0400

    Added patched applied

---
+++ patches/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -O https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff
+curl -O https://dwm.suckless.org/patches/stacker/dwm-stacker-6.2.diff
+curl -O https://dwm.suckless.org/patches/attachaside/dwm-attachaside-6.4.diff
+curl -O https://dwm.suckless.org/patches/swallow/dwm-swallow-6.3.diff
